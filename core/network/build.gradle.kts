e3c7141573e7f577831eba78e8de1ba1d05c6a7e
Oluwadara Abijo <oluwadara.abijo@gmail.com>
2024-12-18 16:00:55 +0100

    feat: Set up network module

---
+++ core/network/build.gradle.kts
@@ -0,0 +1,42 @@
+plugins {
+    alias(libs.plugins.kotlin.serialization)
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
+    id("kotlin-kapt")
+    alias(libs.plugins.hilt)
+}
+
+android {
+    namespace = "com.dara.core.network"
+
+    buildFeatures {
+        buildConfig = true
+    }
+
+    compileSdk = 34
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+
+    defaultConfig {
+        minSdk = 24
+    }
+
+}
+
+dependencies {
+    implementation(libs.retrofit.core)
+    implementation(libs.retrofit.kotlin.serialization)
+    implementation(libs.kotlinx.serialization.json)
+    implementation(libs.coroutines)
+    implementation(libs.okhttp.logging.interceptor)
+    implementation(libs.hilt.android)
+    kapt(libs.hilt.compiler)
+
+}
---
+++ core/network/src/main/kotlin/com/dara/core/network/di/CoreNetworkModule.kt
@@ -0,0 +1,63 @@
+package com.dara.core.network.di
+
+import com.dara.core.network.WeatherApi
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.create
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+internal object CoreNetworkModule {
+
+    @Provides
+    @Singleton
+    fun provideHttpClient(): OkHttpClient {
+        val builder = OkHttpClient.Builder()
+            .connectTimeout(NETWORK_TIMEOUT, TimeUnit.SECONDS)
+            .readTimeout(NETWORK_TIMEOUT, TimeUnit.SECONDS)
+            .writeTimeout(NETWORK_TIMEOUT, TimeUnit.SECONDS)
+
+        builder.addInterceptor(
+            okhttp3.logging.HttpLoggingInterceptor()
+                .apply { setLevel(okhttp3.logging.HttpLoggingInterceptor.Level.BODY) })
+
+        return builder.build()
+    }
+
+    @Provides
+    @Singleton
+    fun providesNetworkJson(): Json = Json {
+        ignoreUnknownKeys = true
+    }
+
+    @Singleton
+    @Provides
+    fun providesRetrofit(
+        networkJson: Json,
+        okHttpClient: OkHttpClient,
+    ): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(
+                networkJson.asConverterFactory("application/json".toMediaType()),
+            )
+            .build()
+    }
+
+    @Provides
+    fun providesWeatherApi(retrofit: Retrofit): WeatherApi = retrofit.create()
+
+}
+
+private const val BASE_URL = "https://api.weatherapi.com/v1/"
+private const val NETWORK_TIMEOUT = 30L
