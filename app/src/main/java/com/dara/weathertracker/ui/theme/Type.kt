ad9cf6eb9985b3f8645b15df1829cdaa556404b5
Oluwadara Abijo <oluwadara.abijo@gmail.com>
2024-12-18 15:11:23 +0100

    style: Set app typography

--- app/src/main/java/com/dara/weathertracker/ui/theme/Type.kt
+++ app/src/main/java/com/dara/weathertracker/ui/theme/Type.kt
@@ -2,14 +2,24 @@ package com.dara.weathertracker.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.dara.weathertracker.R
 
-// Set of Material typography styles to start with
+
+val poppinsFamily = FontFamily(
+    Font(R.font.poppins_light, FontWeight.Light),
+    Font(R.font.poppins_regular, FontWeight.Normal),
+    Font(R.font.poppins_medium, FontWeight.Medium),
+    Font(R.font.poppins_bold, FontWeight.Bold),
+    Font(R.font.poppins_semibold, FontWeight.SemiBold),
+)
+
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = poppinsFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
