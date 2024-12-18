d83c2d47568225102c1bf3f0b067e2265b9d5adf
Oluwadara Abijo <oluwadara.abijo@gmail.com>
2024-12-18 15:13:39 +0100

    feat: Create empty state widget

---
+++ app/src/main/java/com/dara/weathertracker/ui/composables/EmptyStateWidget.kt
@@ -0,0 +1,40 @@
+package com.dara.weathertracker.ui.composables
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight.Companion.Bold
+import androidx.compose.ui.unit.sp
+import com.dara.weathertracker.R
+import com.dara.weathertracker.ui.theme.BlackText
+
+
+@Composable
+fun EmptyStateWidget() {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight(0.7f),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = CenterHorizontally
+    ) {
+        Text(
+            stringResource(R.string.no_city_selected),
+            fontSize = 30.sp,
+            fontWeight = Bold,
+            color = BlackText
+        )
+        Text(
+            stringResource(R.string.please_search_for_a_city),
+            fontSize = 15.sp,
+            fontWeight = Bold,
+            color = BlackText
+        )
+    }
+}
